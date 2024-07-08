9973321e10edbca945367c9e334b3834bec5c0b0
g-kimbell <grahamkimbell@outlook.com>
2024-07-08 10:06:56 +0200

    Add version.py

---
+++ version.py
@@ -0,0 +1,10 @@
+__version__ = '0.1.0'
+__author__ = 'Graham Kimbell'
+__title__ = 'Cucumber'
+__description__ = """
+The perfect pairing for Tomato.
+Tools to distribute jobs, monitor progress and analyse results from Tomato
+battery cycling servers.
+"""
+__url__ = 'https://github.com/g-kimbell/cucumber'
+__license__ = 'MIT'
