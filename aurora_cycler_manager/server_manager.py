1fe3dd0de871086f673e8fc2beaf93b1381da25a
g-kimbell <grahamkimbell@outlook.com>
2024-09-18 10:22:22 +0200

    Bugfix an sql query

--- aurora_cycler_manager/server_manager.py
+++ aurora_cycler_manager/server_manager.py
@@ -655,8 +655,8 @@ class ServerManager:
                 # Update the snapshot status in the database
                 self.execute_sql(
                     "INSERT OR IGNORE INTO results "
-                    "(`Sample ID`, `Last snapshot`) VALUES (?)",
-                    (sample_id)
+                    "(`Sample ID`) VALUES (?)",
+                    (sample_id,)
                 )
                 self.execute_sql(
                     "UPDATE jobs SET `Snapshot status` = ?, `Last snapshot` = ? WHERE `Job ID` = ?",
