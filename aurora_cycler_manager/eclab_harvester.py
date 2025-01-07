02389e5aa762f8a96879a121df053a329f1845f2
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-01-07 16:38:38 +0100

    EC-lab harvester optimisations
    Only converts files that have been modified
    Converts to more efficient hdf5 file
    Cleaner sample ID finding

--- aurora_cycler_manager/eclab_harvester.py
+++ aurora_cycler_manager/eclab_harvester.py
@@ -1,6 +1,6 @@
 """Copyright © 2024, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia.
 
-Harvest EC-lab .mpr files and convert to aurora-compatible gzipped json files.
+Harvest EC-lab .mpr files and convert to aurora-compatible .json.gz / .h5 files.
 
 Define the machines to grab files from in the config.json file.
 
@@ -10,7 +10,7 @@ have been modified since the last time the function was called.
 get_all_mprs does this for all machines defined in the config.
 
 convert_mpr converts an mpr to a dataframe and optionally saves it as a hdf5
-file and/or a gzipped json file. This file contains all cycling data as well as
+file or a gzipped json file. This file contains all cycling data as well as
 metadata from the mpr and information about the sample from the database.
 
 convert_all_mprs does this for all mpr files in the local snapshot folder, and
@@ -26,6 +26,7 @@ import sqlite3
 import sys
 import warnings
 from datetime import datetime
+from pathlib import Path
 
 import h5py
 import numpy as np
@@ -118,6 +119,7 @@ def get_mprs(
 
         # Copy the files using SFTP
         current_datetime = datetime.now()  # Keep time of copying for database
+        new_files = []
         with ssh.open_sftp() as sftp:
             for file in modified_files:
                 # Maintain the folder structure when copying
@@ -128,6 +130,7 @@ def get_mprs(
                     os.makedirs(local_dir)
                 print(f"Copying {file} to {local_path}")
                 sftp.get(file, local_path)
+                new_files.append(local_path)
 
     # Update the database
     with sqlite3.connect(db_path) as conn:
@@ -145,6 +148,8 @@ def get_mprs(
         )
         cursor.close()
 
+        return new_files
+
 def get_all_mprs(force_copy: bool = False) -> None:
     """Get all MPR files from the folders specified in the config.
 
@@ -153,9 +158,10 @@ def get_all_mprs(force_copy: bool = False) -> None:
     dictionaries with the keys "label" and "EC-lab folder location".
     The "label" must match a server in the "Servers" list in the main config.
     """
+    all_new_files = []
     for server in eclab_config["Servers"]:
         server_config = next((s for s in config["Servers"] if s["label"] == server["label"]), None)
-        get_mprs(
+        new_files = get_mprs(
             server["label"],
             server_config["hostname"],
             server_config["username"],
@@ -165,14 +171,14 @@ def get_all_mprs(force_copy: bool = False) -> None:
             eclab_config["Snapshots folder path"],
             force_copy,
         )
+        all_new_files.extend(new_files)
+    return all_new_files
 
 def convert_mpr(
-        sampleid: str,
         mpr_file: str,
-        output_hdf_file: str|None = None,
-        output_jsongz_file: str|None = None,
-        capacity_Ah: float|None = None,
-        ) -> pd.DataFrame:
+        output_jsongz_file: bool = False,
+        output_hdf5_file: bool = True,
+    ) -> tuple[pd.DataFrame, dict]:
     """Convert a ec-lab mpr to dataframe, optionally save as hdf5 or zipped json file.
 
     Args:
@@ -180,7 +186,6 @@ def convert_mpr(
         mpr_file (str): path to the raw mpr file
         output_hdf_file (str, optional): path to save the output hdf5 file
         output_jsongz_file (str, optional): path to save the output zipped json file
-        capacity_Ah (float, optional): capacity of the cell in Ah
 
     Returns:
         pd.DataFrame: DataFrame containing the cycling data
@@ -189,18 +194,17 @@ def convert_mpr(
     - uts: unix timestamp in seconds
     - V (V): Cell voltage in volts
     - I (A): Current in amps
-    - loop_number: number of loops over several techniques
     - cycle_number: number of cycles within one technique
-    - index: index of the method in the payload, not used here
-    - technique: code of technique using Biologic convention, not used here
+    - technique: code of technique using Biologic convention
 
     TODO: use capacity to define C rate
 
     """
     # Normalize paths to avoid escape character issues
     mpr_file = os.path.normpath(mpr_file)
-    output_hdf_file = os.path.normpath(output_hdf_file) if output_hdf_file else None
-    output_jsongz_file = os.path.normpath(output_jsongz_file) if output_jsongz_file else None
+
+    # Try to get the sample ID and run ID from the path
+    run_id, sample_id = get_sampleid_from_mpr(mpr_file)
 
     creation_date = datetime.fromtimestamp(
         os.path.getmtime(mpr_file),
@@ -241,92 +245,136 @@ def convert_mpr(
         (3600 / 1000) * data.data_vars["dq"].to_numpy() /
         np.diff(data.coords["uts"].to_numpy(),prepend=[np.inf])
     )
-    df["loop_number"] = data.data_vars["half cycle"].to_numpy()//2
-    df["cycle_number"] = 0
-    df["index"] = 0
+    df["cycle_number"] = data.data_vars["half cycle"].to_numpy()//2
     df["technique"] = data.data_vars["mode"].to_numpy()
 
-    # If saving files, add metadata, save, update database
-    if output_hdf_file or output_jsongz_file:
-        # get sample data from database
-        try:
-            with sqlite3.connect(db_path) as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT * FROM samples WHERE `Sample ID`=?", (sampleid,))
-                row = cursor.fetchone()
-                columns = [column[0] for column in cursor.description]
-                sample_data = dict(zip(columns, row))
-        except Exception as e:
-            print(f"Error getting job and sample data from database: {e}")
-            sample_data = None
+    # get sample data from database
+    try:
+        with sqlite3.connect(db_path) as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM samples WHERE `Sample ID`=?", (sample_id,))
+            row = cursor.fetchone()
+            columns = [column[0] for column in cursor.description]
+            sample_data = dict(zip(columns, row))
+    except Exception as e:
+        print(f"Error getting job and sample data from database: {e}")
+        sample_data = None
 
-        # Get job data from the snapshot file
-        mpr_metadata = json.loads(data.attrs["original_metadata"])
-        mpr_metadata["job_type"] = "eclab_mpr"
-        yadg_metadata = {k: v for k, v in data.attrs.items() if k.startswith("yadg")}
+    # Get job data from the snapshot file
+    mpr_metadata = json.loads(data.attrs["original_metadata"])
+    mpr_metadata["job_type"] = "eclab_mpr"
+    yadg_metadata = {k: v for k, v in data.attrs.items() if k.startswith("yadg")}
 
-        # Metadata to add
-        timezone = pytz.timezone(config.get("Time zone", "Europe/Zurich"))
-        metadata = {
-            "provenance": {
-                "snapshot_file": mpr_file,
-                "yadg_metadata": yadg_metadata,
-                "aurora_metadata": {
-                    "mpr_conversion" : {
-                        "repo_url": __url__,
-                        "repo_version": __version__,
-                        "method": "eclab_harvester.convert_mpr",
-                        "datetime": datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S %z"),
-                    },
+    # Metadata to add
+    timezone = pytz.timezone(config.get("Time zone", "Europe/Zurich"))
+    metadata = {
+        "provenance": {
+            "snapshot_file": mpr_file,
+            "yadg_metadata": yadg_metadata,
+            "aurora_metadata": {
+                "mpr_conversion" : {
+                    "repo_url": __url__,
+                    "repo_version": __version__,
+                    "method": "eclab_harvester.convert_mpr",
+                    "datetime": datetime.now(timezone).strftime("%Y-%m-%d %H:%M:%S %z"),
                 },
             },
-            "job_data": mpr_metadata,
-            "sample_data": sample_data if sample_data is not None else {},
-        }
+        },
+        "job_data": mpr_metadata,
+        "sample_data": sample_data if sample_data is not None else {},
+    }
 
-        if output_hdf_file:  # Save as hdf5
-            folder = os.path.dirname(output_hdf_file)
-            if not folder:
-                folder = "."
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            df.to_hdf(
-                output_hdf_file,
-                key="cycling",
-                complib="blosc",
-                complevel=2,
-            )
-            # Open the HDF5 file with h5py and add metadata
-            with h5py.File(output_hdf_file, "a") as file:
-                if "cycling" in file:
-                    file["cycling"].attrs["metadata"] = json.dumps(metadata)
-                else:
-                    print("Dataset 'cycling' not found.")
+    if output_hdf5_file or output_jsongz_file:  # Save and update database
+        if not sample_id:
+            print(f"Not saving {mpr_file}, no valid Sample ID found")
+            return df, metadata
+        folder = Path(config["Processed snapshots folder path"]) / run_id / sample_id
+        if not folder.exists():
+            folder.mkdir(parents=True)
 
+        mpr_filename = Path(mpr_file).name
         if output_jsongz_file:  # Save as zipped json
-            folder = os.path.dirname(output_jsongz_file)
-            if not folder:
-                folder = "."
-            if not os.path.exists(folder):
-                os.makedirs(folder)
-            with gzip.open(output_jsongz_file, "wt") as f:
+            jsongz_filepath = folder / ("snapshot." + mpr_filename.replace(".mpr", ".json.gz"))
+            with gzip.open(jsongz_filepath, "wt") as f:
                 json.dump({"data": df.to_dict(orient="list"), "metadata": metadata}, f)
+
+        if output_hdf5_file:  # Save as hdf5
+            hdf5_filepath = folder / ("snapshot." + mpr_filename.replace(".mpr", ".h5"))
+            # Ensure smallest data types are used
+            df = df.astype({"V (V)": "float32", "I (A)": "float32"})
+            df = df.astype({"technique": "int16", "cycle_number": "int32"})
+            df.to_hdf(
+                hdf5_filepath,
+                key="data",
+                mode="w",
+                complib="blosc",
+                complevel=9,
+            )
+            # create a dataset called metadata and json dump the metadata
+            with h5py.File(hdf5_filepath, "a") as f:
+                f.create_dataset("metadata", data=json.dumps(metadata))
 
         # Update the database
         with sqlite3.connect(db_path) as conn:
             cursor = conn.cursor()
             cursor.execute(
                 "INSERT OR IGNORE INTO results (`Sample ID`) VALUES (?)",
-                (sampleid,),
+                (sample_id,),
             )
             cursor.execute(
                 "UPDATE results "
                 "SET `Last snapshot` = ? "
                 "WHERE `Sample ID` = ?",
-                (creation_date, sampleid),
+                (creation_date, sample_id),
             )
             cursor.close()
-    return df
+    return df, metadata
+
+def get_sampleid_from_mpr(mpr_rel_path: str) -> str:
+    """Try to get the sample ID based on the remote file path."""
+    # split the relative path into parts
+    parts = Path(mpr_rel_path).parts
+
+    run_id_lookup = eclab_config.get("Run ID lookup", {})
+    db_path = config["Database path"]
+
+    # Usually the run_ID is the 2nd parent folder and the sample number is the level above
+    # If this is not the case, try the 3rd parent folder and 1st parent folder
+    for i in [-3, -4, -2]:
+        try:
+            run_folder = parts[i]
+            sample_folder = parts[i+1]
+        except IndexError as e:
+            if i == -2:
+                msg = f"Could not get run and sample number components for {mpr_rel_path}"
+                raise ValueError(msg) from e
+            continue
+        run_id = run_id_lookup.get(run_folder, None)
+        if not run_id:
+            with sqlite3.connect(db_path) as conn:
+                cursor = conn.cursor()
+                cursor.execute("SELECT `Run ID` FROM samples WHERE `Sample ID` LIKE ?", (f"%{run_folder}%",))
+                result = cursor.fetchone()
+                cursor.close()
+            if result:
+                run_id = result[0]
+        if run_id:
+            try:
+                sample_number = int(sample_folder)
+            except ValueError:
+                match = re.search(r"cell(\d+)[_(]?", sample_folder)
+                sample_number = int(match.group(1)) if match else None
+            if sample_number:
+                break
+
+    if not run_id:
+        msg = f"Could not find a Run ID for {mpr_rel_path}"
+        raise ValueError(msg)
+    if not sample_number:
+        msg = f"Could not find a sample number for {mpr_rel_path}"
+        raise ValueError(msg)
+    sample_id = f"{run_id}_{sample_number:02d}"
+    return run_id, sample_id
 
 def convert_all_mprs() -> None:
     """Convert all raw .mpr files to gzipped json files.
@@ -336,94 +384,26 @@ def convert_all_mprs() -> None:
     "EC lab harvester" can optionally contain "Run ID lookup" with a dictionary of run ID
     lookups for folders that are named differently to run ID on the server.
     """
+    # walk through raw_folder and get the sample ID
     raw_folder = eclab_config["Snapshots folder path"]
-    processed_folder = config["Processed snapshots folder path"]
-    db_path = config["Database path"]
-
-    # Lookup dict for folder_name: run_id
-    # In case folders are named differently to run_id on the server
-    run_id_lookup = eclab_config.get("Run ID lookup", {})
-
-    for run_folder in os.listdir(raw_folder):
-        print("Processing folder", run_folder)
-        if not os.path.isdir(os.path.join(raw_folder, run_folder)):
-            print(f"Skipping {run_folder} - not a folder")
-            continue
-        run_id = run_id_lookup.get(run_folder, None)
-
-        # Try looking up in the database if not found in the lookup table
-        if not run_id:
-            with sqlite3.connect(db_path) as conn:
-                cursor = conn.cursor()
-                cursor.execute("SELECT `Run ID` FROM samples WHERE `Sample ID` LIKE ?", (f"%{run_folder}%",))
-                result = cursor.fetchone()
-                cursor.close()
-            if result:
-                run_id = result[0]
-                print(f"Found Run ID '{run_id}' for {run_folder}")
-            else:
-                print(f"Skipping {run_folder} - could not find a Run ID")
-                continue
-        print(f"Found Run ID '{run_id}'")
-
-        # Check for loose .mpr in folder
-        mprs = [f for f in os.listdir(os.path.join(raw_folder, run_folder)) if f.endswith(".mpr")]
-        if mprs:
-            for i,mpr in enumerate(mprs):
-                mpr_path = os.path.join(raw_folder, run_folder, mpr)
-                match = re.search(r"cell(\d+)[_(]?", mpr)
-                if match:
-                    sample_number = int(match.group(1))
-                else:
-                    print(f"Skipping {mpr} - could not recognise a sample number")
-                    continue
-                sample_id = f"{run_id}_{sample_number:02d}"
-                print(f"Processing {sample_id}")
-
-                # Convert the mpr to hdf
-                mpr_path = os.path.join(raw_folder, run_folder, mpr)
-                output_jsongz = os.path.join(processed_folder, run_id, sample_id, f"snapshot.mpr-{i}.json.gz")
+    for _dirpath, _dirnames, filenames in os.walk(raw_folder):
+        for filename in filenames:
+            if filename.endswith(".mpr"):
                 try:
-                    convert_mpr(sample_id, mpr_path, None, output_jsongz, 0.00154)
-                except Exception as e:
-                    print(f"Error processing {mpr}: {e}")
-
-        # Check for sample folders
-        for sample_folder in os.listdir(os.path.join(raw_folder, run_folder)):
-            root_folder = os.path.join(raw_folder, run_folder, sample_folder)
-            if not os.path.isdir(root_folder):
-                continue
-            try:
-                sample_number = int(sample_folder)
-            except ValueError:
-                match = re.search(r"cell(\d+)[_(]?", sample_folder)
-                if match:
-                    sample_number = int(match.group(1))
-                else:
-                    print(f"Skipping {sample_folder} - could not recognise a sample number")
+                    convert_mpr(
+                        mpr_path,
+                        output_jsongz_file=False,
+                        output_hdf5_file=True,
+                    )
+                except (ValueError, IndexError) as e:
+                    print(f"Error getting sample ID: {e}")
                     continue
-            sample_id = f"{run_id}_{sample_number:02d}"
-            print(f"Processing {sample_id}")
-
-            # Walk through the sample folder, find all .mpr files including in subfolders
-            mprs = []
-            for dirpath, _dirnames, filenames in os.walk(root_folder):
-                for filename in filenames:
-                    if filename.endswith(".mpr"):
-                        mprs.append(os.path.relpath(os.path.join(dirpath, filename), root_folder))
-            if not mprs:
-                print(f"No mpr files found for {sample_id}")
-                continue
-
-            # Convert the mpr to hdf
-            for i,mpr in enumerate(mprs):
-                mpr_path = os.path.join(root_folder, mpr)
-                output_jsongz = os.path.join(processed_folder, run_id, sample_id, f"snapshot.mpr-{i}.json.gz")
-                try:
-                    convert_mpr(sample_id, mpr_path, None, output_jsongz, 0.00154)
-                except Exception as e:
-                    print(f"Error processing {mpr}: {e}")
 
 if __name__ == "__main__":
-    get_all_mprs()
-    convert_all_mprs()
+    new_files = get_all_mprs()
+    for mpr_path in new_files:
+        convert_mpr(
+            mpr_path,
+            output_jsongz_file=False,
+            output_hdf5_file=True,
+        )
