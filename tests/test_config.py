d65774c8efdda0d1f8dc8abf9eda8bb77eea70e4
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-04-17 14:47:07 +0200

    Update config testing

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,11 +2,27 @@
 
 from pathlib import Path
 
-from aurora_cycler_manager.config import get_config
+from aurora_cycler_manager.config import _read_config_file, get_config
 
 
 class TestGetConfig:
-    """Test the get_config function."""
+    """Test the get_config and _read_config_file functions."""
+
+    def test_read_config_file(self) -> None:
+        """Test the _read_config_file function."""
+        config = _read_config_file()
+        assert isinstance(config, dict)
+        assert "Shared config path" in config
+        assert "SSH private key path" in config
+        assert "Snapshots folder path" in config
+
+    def check_file_read_once(self) -> None:
+        """Check that all configs are the same object in memory."""
+        config1 = get_config()
+        config2 = get_config()
+        assert config1 is config2
+        config3 = get_config(reload=True)
+        assert config1 is not config3
 
     def test_get_config(self) -> None:
         """Test the get_config function."""
@@ -21,13 +37,6 @@ class TestGetConfig:
         config = get_config()
         assert isinstance(config, dict)
         assert all(isinstance(k, str) for k in config)
-        path_keys = [
-            "Shared config path",
-            "SSH private key path",
-            "Database path",
-            "Database backup folder path",
-            "Processed snapshots folder path",
-        ]
-        assert all(key in config for key in path_keys)
+        path_keys = [k for k in config if "path" in k.lower()]
         assert all(isinstance(config[key], Path) for key in path_keys)
         assert all(config[key].is_absolute() for key in path_keys)
