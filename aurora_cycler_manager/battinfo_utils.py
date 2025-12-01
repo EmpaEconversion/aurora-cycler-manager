7c0c09feb259a29b7277e4dd597861274180d37e
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2025-12-01 17:30:25 +0100

    Add Zenodo info upload

--- aurora_cycler_manager/battinfo_utils.py
+++ aurora_cycler_manager/battinfo_utils.py
@@ -1,8 +1,14 @@
 """BattINFO ontology functions."""
 
+import base64
+import io
 import json
 import logging
 from copy import deepcopy
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -469,3 +475,354 @@ def generate_battery_test(ontologized_protocols: dict | list[dict]) -> dict:
         },
         "hasMeasurementParameter": recursive_protocol(ontologized_protocols),
     }
+
+
+def generate_basic_output_jsonld() -> dict:
+    """Generate basic 'hasOutput' key for BatteryTest."""
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "@type": ["BatteryTestResult", "dcat:Dataset"],
+            "dcat:keyword": ["battery", "R2032", "coin cell"],
+        },
+    }
+
+
+def add_ccid_output(
+    ccid: str,
+) -> dict:
+    """Add CCID to output section of json-ld output."""
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "dc:title": f"Cycling data coin cell {ccid}",
+            "dc:description": f"Cycling data coin cell {ccid}",
+        },
+    }
+
+
+def add_data(
+    rel_file_path: str,
+    zenodo_doi_url: str | None,
+) -> dict:
+    """Add links to data files to output section of json-ld output.
+
+    Args:
+        rel_file_path: relative path to file, e.g. empa__ccid01345/empa__ccid01345.bdf.parquet
+        zenodo_doi_url: path to zenodo archive e.g. https://doi.org/10.1234/zenodo.12345678
+
+    Returns:
+        dict with "BatteryTest" as top level type.
+
+    """
+    if rel_file_path.endswith(".parquet"):
+        additions = {
+            "dcat:mediaType": "application/vnd.apache.parquet",
+            "csvw:tableSchema": "https://w3id.org/battery-data-alliance/ontology/battery-data-format/schema",
+        }
+    elif rel_file_path.endswith(".csv"):
+        additions = {
+            "dcat:mediaType": "text/csv",
+            "csvw:tableSchema": "https://w3id.org/battery-data-alliance/ontology/battery-data-format/schema",
+            "csvw:dialect": {"@type": "csvw:Dialect", "csvw:delimiter": ",", "csvw:skipRows": 0},
+        }
+    elif rel_file_path.endswith(".h5"):
+        # Not a BDF but lets add it anyway
+        additions = {
+            "dcat:mediaType": "application/x-hdf5",
+        }
+    elif rel_file_path.endswith(".json"):
+        additions = {
+            "dcat:mediaType": "application/json",
+        }
+    else:
+        msg = f"Unknown file type: {rel_file_path}"
+        raise ValueError(msg)
+
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "dcat:distribution": {
+                "@id": f"{zenodo_doi_url}#{rel_file_path}" if zenodo_doi_url else rel_file_path,
+                "@type": "dcat:Distribution",
+                **additions,
+            }
+        },
+    }
+
+
+def add_zenodo_url(
+    zenodo_doi_url: str,
+) -> dict:
+    """Add Zenodo URL to output section of json-ld output."""
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "dcat:accessURL": zenodo_doi_url,
+            "dcat:endpointURL": f"https://zenodo.org/api/records/{zenodo_doi_url.split('.')[-1]}",
+        },
+    }
+
+
+def add_associated_media(
+    paper_doi_url: str,
+    sample_to_fig: dict,
+    ccid: str,
+    sample_id: str,
+) -> dict:
+    """Add associated media to output section of json-ld output."""
+    figs = [v for k, v in sample_to_fig.items() if k in (sample_id, ccid)]  # e.g. ["Fig. 3a", "Fig. 3b"]
+    if not figs:
+        return {"@type": "BatteryTest"}
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "schema:associatedMedia": {
+                "@id": paper_doi_url,
+                "rdfs:label": figs[0] if len(figs) == 1 else figs,
+                "rdfs:comment": "Subfigure of associated scientific publication containing this data",
+            }
+        },
+    }
+
+
+def add_citation(
+    citation_string: str,
+) -> dict:
+    """Add publishing details to output section of json-ld output."""
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "schema:citation": citation_string,
+            "dc:license": "https://creativecommons.org/licenses/by/4.0/",
+            "dc:issued": datetime.now().date().isoformat(),  # noqa: DTZ005
+            "schema:datePublished": datetime.now().date().isoformat(),  # noqa: DTZ005
+        },
+    }
+
+
+def add_authors(
+    authors: list[dict],
+    institutions: dict[str, dict],
+) -> dict:
+    """Add authors to output section of json-ld.
+
+    Args:
+        authors: a list of dictionaries, each describing one author
+            the dict must include the keys:
+                "orcid": str e.g. https://orcid.org/1234-1234-1234-1234
+                "name": str e.g. "John Battery"
+                "affiliation": list[str] e.g. ["Empa", "ETH Zurich", "EPFL"]
+        institutions: a dict of dictionaries, each describing one institution
+            e.g.
+                {
+                    "Empa": {"wikidata_url": "https://www.wikidata.org/wiki/Q683116"}
+                    "ETH Zurich": {"wikidata_url": "https://www.wikidata.org/wiki/Q11942"}
+                    "EPFL": {"wikidata_url": "https://www.wikidata.org/wiki/Q262760"}
+                }
+
+    Returns:
+        dict with "BatteryTest" as top level type.
+
+    """
+    authors_jsonld = [
+        {
+            "@type": "schema:Person",
+            "@id": author["orcid"],
+            "schema:name": author["name"],
+            "schema:affiliation": [
+                {
+                    "@type": "schema:ResearchOrganization",
+                    "@id": institutions[affil]["wikidata_url"],
+                    "schema:name": affil,
+                }
+                for affil in author["affiliation"]
+            ]
+            if len(author["affiliation"]) > 1
+            else {
+                "@type": "schema:ResearchOrganization",
+                "@id": institutions[author["affiliation"][0]]["wikidata_url"],
+                "schema:name": author["affiliation"][0],
+            },
+        }
+        for author in authors
+    ]
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "dc:creator": authors_jsonld[0] if len(authors_jsonld) == 1 else authors_jsonld,
+        },
+    }
+
+
+def add_institution(
+    name: str = "Empa",
+    wikidata_url: str = "https://www.wikidata.org/wiki/Q683116",
+) -> dict:
+    """Add publishing details to output section of json-ld."""
+    return {
+        "@type": "BatteryTest",
+        "hasOutput": {
+            "dc:publisher": {
+                "@type": "schema:ResearchOrganization",
+                "@id": wikidata_url,
+                "schema:name": name,
+            },
+        },
+    }
+
+
+def parse_zenodo_info_xlsx(
+    xlsx_file: str | Path,
+) -> dict:
+    """Parse zenodo info from uploaded xlsx content string.
+
+    Args:
+        xlsx_file: Path to a file, a content string from upload button
+
+    Returns:
+        dict with zenodo info
+
+    """
+    if isinstance(xlsx_file, Path):
+        excel_file = pd.ExcelFile(xlsx_file)
+    else:
+        _content_type, content_string = xlsx_file.split(",")
+        decoded = base64.b64decode(content_string)
+        excel_file = pd.ExcelFile(io.BytesIO(decoded))
+    sheet_names = [str(s) for s in excel_file.sheet_names]
+    expected_sheets = ["General", "Figures", "Authors", "Institutions"]
+    if not all(sheet in expected_sheets for sheet in sheet_names):
+        msg = "Uploaded xlsx does not have expected sheets for Zenodo info."
+        raise ValueError(msg)
+
+    # Read General sheet
+    general_df = pd.read_excel(excel_file, sheet_name="General", engine="openpyxl", header=None)
+    keys = general_df[0].to_numpy()
+    keys = [k.lower().replace(" ", "_") for k in keys]
+    values = general_df[1].to_numpy()
+    general_dict = dict(zip(keys, values, strict=True))
+
+    # Read Figures sheet
+    figures_df = pd.read_excel(excel_file, sheet_name="Figures", engine="openpyxl")
+    figures_dict = {}
+    for _, row in figures_df.iterrows():
+        key = row["Barcode"] if pd.notna(row["Barcode"]) else row["Sample ID"]
+        figures = row.drop(labels=["Sample ID", "Barcode"]).dropna().astype(str).tolist()
+        figures_dict[key] = figures
+
+    # Read Institutions sheet
+    institutions_df = pd.read_excel(excel_file, sheet_name="Institutions", engine="openpyxl")
+    institutions_df.columns = [col.lower().replace(" ", "_") for col in institutions_df.columns]
+    institutions_dict = institutions_df.set_index("name").to_dict(orient="index")
+
+    # Read Authors sheet
+    authors_df = pd.read_excel(excel_file, sheet_name="Authors", engine="openpyxl")
+    authors_list = []
+    for _, row in authors_df.iterrows():
+        affil_list = row.drop(labels=["ORCID", "Name"]).dropna().astype(str).tolist()
+        authors_list.append(
+            {
+                "name": row["Name"],
+                "orcid": row["ORCID"],
+                "affiliation": affil_list,
+            }
+        )
+
+    return {
+        **general_dict,
+        "sample_to_fig": figures_dict,
+        "institutions": institutions_dict,
+        "authors": authors_list,
+    }
+
+
+def generate_zenodo_info_xlsx_template(
+    sample_ids: list[str],
+    ccids: list[str],
+) -> io.BytesIO:
+    """Generate a template excel file for zenodo info.
+
+    Args:
+        sample_ids: list of sample IDs
+        ccids: list of CCIDs
+
+    Returns:
+        pd.DataFrame with template data
+
+    """
+    general_sheet = {
+        "Citation string": "Enter citation here, like Author et al., Journal, Year",
+        "Zenodo DOI URL": "Enter Zenodo link here, e.g. https://doi.org/10.1234/zenodo.12345678",
+        "Publication DOI URL": "Enter Publication DOI link here, e.g. https://doi.org/10.1234/journal.12345678",
+    }
+    figures_sheet = {
+        "Sample ID": sample_ids,
+        "Barcode": ccids,
+        "Figures": [None] * len(sample_ids),
+    }
+    authors_sheet = {
+        "Name": ["Firstname Lastname"],
+        "ORCID": ["https://orcid.org/0000-0000-0000-0000"],
+        "Affiliations": ["Must be a name from the institutions sheet"],
+    }
+    institutions_sheet = [
+        {"Name": "Empa", "Wikidata URL": "https://www.wikidata.org/wiki/Q683116"},
+        {"Name": "ETH Zurich", "Wikidata URL": "https://www.wikidata.org/wiki/Q11942"},
+        {"Name": "EPFL", "Wikidata URL": "https://www.wikidata.org/wiki/Q262760"},
+        {"Name": "University of Bern", "Wikidata URL": "https://www.wikidata.org/wiki/Q659080"},
+        {"Name": "UNIL", "Wikidata URL": "https://www.wikidata.org/wiki/Q658975"},
+        {"Name": "University of Geneva", "Wikidata URL": "https://www.wikidata.org/wiki/Q503473"},
+    ]
+    buffer = io.BytesIO()
+    with pd.ExcelWriter(buffer, engine="xlsxwriter") as writer:
+        workbook = writer.book
+        title_settings = {"align": "left", "top": 1, "bottom": 1, "bold": True}
+        title_format = workbook.add_format({**title_settings, "left": 1, "right": 1})
+        left_title_format = workbook.add_format({**title_settings, "left": 1, "right": 0})
+        middle_title_format = workbook.add_format({**title_settings, "left": 0, "right": 0})
+        right_title_format = workbook.add_format({**title_settings, "left": 0, "right": 1})
+
+        pd.DataFrame.from_dict(general_sheet, orient="index", columns=["Value"]).to_excel(
+            writer, sheet_name="General", header=False
+        )
+        worksheet = writer.sheets["General"]
+        worksheet.write("A1", "Citation string", title_format)
+        worksheet.write("A2", "Zenodo DOI URL", title_format)
+        worksheet.write("A3", "Publication DOI URL", title_format)
+        worksheet.autofit()
+
+        pd.DataFrame.from_dict(figures_sheet).to_excel(writer, sheet_name="Figures", index=False)
+        worksheet = writer.sheets["Figures"]
+        worksheet.autofit()
+        worksheet.set_column("C:L", 10)
+        worksheet.write("A1", "Sample ID", title_format)
+        worksheet.write("B1", "Barcode", title_format)
+        worksheet.write("C1", "Figures", left_title_format)
+        worksheet.write_row("D1:K1", [""] * 8, middle_title_format)
+        worksheet.write("L1", "", right_title_format)
+        worksheet.write("C2", "Fig. 1a")
+        worksheet.write("D2", "Fig. 2b")
+
+        pd.DataFrame.from_dict(authors_sheet).to_excel(writer, sheet_name="Authors", index=False)
+        worksheet = writer.sheets["Authors"]
+        worksheet.autofit()
+        worksheet.set_column("C:L", 16)
+        worksheet.write("A1", "Name", title_format)
+        worksheet.write("B1", "ORCID", title_format)
+        worksheet.write("C1", "Affiliations", left_title_format)
+        worksheet.write_row("D1:I1", [""] * 6, middle_title_format)
+        worksheet.write("J1", "", right_title_format)
+        worksheet.write("A3", "Corsin Battaglia")
+        worksheet.write("B3", "https://orcid.org/0000-0002-5003-1134")
+        worksheet.write("C3", "Empa")
+        worksheet.write("D3", "ETH Zurich")
+        worksheet.write("E3", "EPFL")
+
+        pd.DataFrame(institutions_sheet).to_excel(writer, sheet_name="Institutions", index=False)
+        worksheet = writer.sheets["Institutions"]
+        worksheet.autofit()
+        worksheet.write("A1", "Name", title_format)
+        worksheet.write("B1", "Wikidata URL", title_format)
+    buffer.seek(0)
+    return buffer
