80d824b9cb41915c01a4e40e9db866110f8c9124
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-04-14 18:14:40 +0200

    Separate log levels for aurora and other packages

--- aurora_cycler_manager/setup_logging.py
+++ aurora_cycler_manager/setup_logging.py
@@ -7,15 +7,20 @@ import logging
 import sys
 
 
-def setup_logging(level: int = logging.INFO) -> None:
+def setup_logging(level: int = logging.WARNING, aurora_level: int = logging.INFO) -> None:
     """Set up logging config."""
     for handler in logging.root.handlers[:]:  # Remove existing handlers
         logging.root.removeHandler(handler)
 
+    # Aurora should be at least the general level
+    aurora_level = min(aurora_level, level)
+
+    # For all other packages
     logging.basicConfig(
         level=level,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-        handlers=[
-            logging.StreamHandler(sys.stdout),
-        ],
+        handlers=[logging.StreamHandler(sys.stdout)],
     )
+
+    # For aurora logging
+    logging.getLogger("aurora_cycler_manager").setLevel(aurora_level)
