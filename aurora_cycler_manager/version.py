9c257f4f701ed6185d12383066df423976bbafd9
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-05-07 18:00:44 +0200

    Bump version v0.11.1 -> v0.11.2.

--- aurora_cycler_manager/version.py
+++ aurora_cycler_manager/version.py
@@ -1,6 +1,6 @@
 """Version information for the Aurora cycler manager package."""
 
-__version__ = "0.11.1"
+__version__ = "0.11.2"
 __author__ = "Graham Kimbell"
 __title__ = "Aurora cycler manager"
 __license__ = "MIT"
