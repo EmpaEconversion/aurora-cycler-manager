27bc32d3a941e35ff3069d1c0a23a0637707b1a5
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-08-21 16:56:50 +0200

    Switch to uv publish (#182)
    * Switch to UV publish
    
    * Move dev deps to dependency-groups

--- aurora_cycler_manager/version.py
+++ aurora_cycler_manager/version.py
@@ -1,13 +1,14 @@
 # Copyright © 2025-2026, Empa.
 """Version information for the Aurora cycler manager package."""
 
-__version__ = "0.11.6"
+from importlib.metadata import version
+
+__version__ = version("aurora-cycler-manager")
 __author__ = "Graham Kimbell"
 __title__ = "Aurora cycler manager"
 __license__ = "MIT"
 __copyright__ = "2025, Empa, Graham Kimbell, Enea Svaluto-Ferro, Ruben Kuhnel, Corsin Battaglia"
 __description__ = """
-Tools to distribute jobs, monitor progress and analyse results from Tomato
-battery cycling servers.
+Battery cycler management, data analysis and visualisation.
 """
 __url__ = "https://github.com/EmpaEconversion/aurora-cycler-manager"
